27e83ca36501b9ecefb0eae84ac50b70b204c2b0
vpsanderklo <sanderkloj@gmail.com>
2022-03-25 13:49:21 +0200

    Adding new tests

--- test_product_page.py
+++ test_product_page.py
@@ -18,3 +18,23 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.click_to_button_add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_compare_product_in_basket()
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_to_button_add_to_basket()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_to_button_add_to_basket()
+    page.should_disapiered_success_message()
